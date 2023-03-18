a868e0b8f1b9e06b36b3ada38534bca9a447a549
MchSasha <sashamoysevich@gmail.com>
2023-03-18 13:16:21 +0300

    PR

---
+++ LR1/constants.py
@@ -0,0 +1,35 @@
+class Constants:
+    # initial data
+    MAX_SPACE = 40
+    WATER_SUPPLY = 50
+    SOLAR_ENERGY_SUPPLY = 50
+    IMMUNITY_SUPPLY = 50
+    DAMAGE_SUPPLY = 50
+    FRUIT_TREE_FERTILITY = 100
+
+    # benefits
+    RAIN_BENEFIT = 25
+    WATERING_BENEFIT = 25
+    SOLAR_ENERGY_BENEFIT = 20
+    FERTILIZER_BENEFIT = 50
+    CARE_BENEFIT = 25
+
+    # waste
+    WATER_USAGE = 5
+    SOLAR_ENERGY_USAGE = 5
+    IMMUNITY_USAGE = 5
+    DAMAGE = 5
+
+    # time
+    TREE_BLOSSOM_DELAY = 5
+    SEED_BLOSSOM_DELAY = 5
+    FRUIT_LIFE = 15
+
+    # blossom minimum
+    WATER_MINIMUM = 20
+    SOLAR_ENERGY_MINIMUM = 20
+    IMMUNITY_MINIMUM = 20
+    DAMAGE_MINIMUM = 20
+
+    # misery conditions
+    WRECkER_MULTIPLYING_DELAY = 10
