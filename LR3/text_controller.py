ff752bb8255792d9f2efabc993f442f5c86c7792
Vasilich563 <92030020+Vasilich563@users.noreply.github.com>
2023-03-31 21:27:11 +0300

    upload all files
    Якубовский Кирилл Васильевич, 121703, л.р.3

---
+++ LR3/text_controller.py
@@ -0,0 +1,30 @@
+# Author Vodohleb04
+from typing import NoReturn
+
+import pygame
+
+
+class TextObject:
+
+    def __init__(self, x, y, text_func, color, font_name, font_size):
+        self._pos = (x, y)
+        self._text_func = text_func
+        self._color = color
+        self._font = pygame.font.SysFont(font_name, font_size)
+        self._bounds = self.get_surface(text_func())
+
+    def draw(self, surface, centralized=False) -> NoReturn:
+        text_surface, self._bounds = self.get_surface(self._text_func())
+        pos = (self._pos[0] - self._bounds.width // 2, self._pos[1]) if centralized else self._pos
+        surface.blit(text_surface, pos)
+
+    def get_surface(self, text):
+        text_surface = self._font.render(text, False, self._color)
+        return text_surface, text_surface.get_rect()
+
+    def update(self):
+        pass
+
+    @property
+    def text_string(self):
+        return self._text_func()
