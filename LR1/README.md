5c915ff87f6accb1a10315785347101a7ac6d16c
Hleb <fosterlounch@gmail.com>
2023-04-14 09:46:15 +0300

    add LR1

---
+++ LR1/README.md
@@ -0,0 +1,5 @@
+# ППОИС
+
+### Лабораторная работа №1
+![image](https://user-images.githubusercontent.com/91974808/218490773-855b7f65-c9ed-46d1-973b-2fca9c544ff3.png)
+![image](https://user-images.githubusercontent.com/91974808/218490879-eb228d2b-4cc1-45ea-8c3c-20f5334800af.png)
--- LR1/main.py
+++ LR1/main.py
@@ -1,143 +1,18 @@
-from Make.Make import Make
-from Run.Run import Run
-import click
-import copy
+import field
+import argparse
 
-@click.command()
-@click.option('-w', required=True, help='File for way')
-@click.option('-p', required=True, help='File for products')
-@click.option('-s', required=True, help='Start')
-@click.option('-m', default='', help='For automatic operation')
-@click.option('-d', default=0, help='How many day')
-@click.option('-n', default=False, help='Ned generate new trains/station?')
-def main(p, w, s, m, d, n):
-    # Читаем пути
-    f = open(w, 'r')
-    way = []
-    w = []
-    temp = f.readlines()
-    for i in temp:
-        for j in range(len(i)):
-            if not i[j] == '\n' and not i[j] == ',' and not i[j] == ' ':
-                w.append(int(i[j]))
-        way.append(list(w))
-        w.clear()
-    f.close()
+parser = argparse.ArgumentParser(prog='game of life')
 
-    # Читаем продукты
-    f = open(p)
-    products = f.readlines()
-    for i in range(len(products)):
-       products[i] = products[i][0:len(products[i]) - 1]
-    f.close()
+parser.add_argument('filename')
+parser.add_argument('-c', '--count', type=int, required=True, help='count of steps to produce')
+parser.add_argument('-v', '--verbose', action='store_true', help='print field on each step')
+parser.add_argument('-o', '--output', action='store', help='output file')
 
-    if n == False:
-        #Чтение поездов из файла
-        f = open('trains')
-        lines = f.read().splitlines()
-        f.close()
+args = parser.parse_args()
 
-        dic = {}
-
-        trains = []
-
-        for line in lines:
-            if line == '===':
-                dic_1 = copy.deepcopy(dic)
-                trains.append(dic_1)
-                dic.clear()
-                continue
-            temp = line.split(';')
-            key = temp[0]
-            value = temp[1]
-            dic.update({key: value})
-
-        for temp in trains:
-            data_dict = eval(temp['train'])
-            temp['train'] = data_dict
-            data_list = []
-            for i in temp['list']:
-                if not i == '[' and not i == ']' and not i == ' ' and not i == ',':
-                    data_list.append(int(i))
-            temp['list'] = data_list
-            if temp['make'] == 'True':
-                temp['make'] = True
-            else:
-                temp['make'] = False
-        #Чтение станции из файла
-        f = open('station')
-        lines = f.read().splitlines()
-        f.close()
-
-        dic = {}
-        station = []
-
-        for line in lines:
-            if line == '===':
-                dic_1 = copy.deepcopy(dic)
-                station.append(dic_1)
-                dic.clear()
-                continue
-            temp = line.split(';')
-            key = temp[0]
-            value = temp[1]
-            dic.update({key: value})
-
-        for temp in station:
-            a = int(temp['number'])
-            temp['number'] = a
-            data_dict = eval(temp['storage'])
-            temp['storage'] = data_dict
-            data_list = []
-            for i in temp['queue']:
-                if not i == '[' and not i == ']' and not i == ',' and not i == ' ':
-                    data_list.append(i)
-            temp['queue'] = data_list
-            data_list1 = []
-            for i in temp['roads']:
-                if not i == '[' and not i == ']' and not i == ',' and not i == ' ':
-                    data_list1.append(int(i))
-            temp['roads'] = data_list1
-            b = int(temp['train_number'])
-            temp['train_number'] = b
-
-        task = str(str(s) + ' ' + str(m) + ' ' + str(d))
-        run = Run(station, trains, task)
-        run.handler()
-        # Запись поездоф в файл
-        f = open('trains', 'w')
-        for i in run.train_list:
-            for key, item in i.items():
-                f.write('{};{}\n'.format(key, item))
-            f.write("===\n")
-        f.close()
-        # Запись станций в файл
-        f = open('station', 'w')
-        for i in run.station_list:
-            for key, value in i.items():
-                f.write('{};{}\n'.format(key, value))
-            f.write('===\n')
-        f.close()
-
-    else:
-        make = Make(products, way)
-        task = str(str(s) + ' ' + str(m) + ' ' + str(d))
-        run = Run(make.Make_Station_list(), make.Make_Trane_list(), task)
-        run.handler()
-        # Запись поездов в файл
-        f = open('trains', 'w')
-        for i in run.train_list:
-            for key, item in i.items():
-                f.write('{};{}\n'.format(key, item))
-            f.write("===\n")
-        f.close()
-        # Запись станций в файл
-        f = open('station', 'w')
-        for i in run.station_list:
-            for key, value in i.items():
-                f.write('{};{}\n'.format(key, value))
-            f.write('===\n')
-        f.close()
-
-if __name__ == '__main__':
-    main()
+f = field.field(args.filename, args.output)
+for _ in range(args.count):
+    f.process()
+    if args.verbose:
+        print(f, end='\n\n')
+f.dump()
