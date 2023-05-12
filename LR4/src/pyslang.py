d00457edf079649f95cd3c50a4a7721463c871f9
Rastsislau <ungaf@ya.ru>
2023-05-12 09:47:27 +0300

    LW4

---
+++ LR4/src/pyslang.py
@@ -0,0 +1,82 @@
+#! /bin/python3.10 
+
+# THIS FILE IS A PART OF SLANG INTERPRETER.
+# AUTHOR: Rostislav Lipsky (https://github.com/ungaf)
+# DATE: 06.02.2023
+# VISIT https://github.com/ungaf/slang AND READ THE LICENSE BEFORE USING.
+
+
+import os
+import pickle
+import json
+import argparse
+from cli2gui import Cli2Gui
+from pprint import pprint
+from slang import Compiler, Interpreter
+
+
+def save_pickled(_object, path: str = "out.sx"):
+    with open(path, 'wb') as com_file:
+        com_file.write(pickle.dumps(_object))
+
+
+def open_pickled(path: str):
+    with open(path, 'rb') as src_file:
+        macros = pickle.loads(src_file.read())
+    return macros
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=
+                                     """
+                                     pyslang is a slang interpreter. Slang source code files have .slang expansion
+                                     and slang bytecode (compiled programs) have .sx expansion.
+                                     """)
+    parser.add_argument("command",
+                        choices=["run", "com", "exe"],
+                        help="mode (run - interpret, com - compile, exe - execute bytecode)")
+    parser.add_argument("file", type=str, help="path to source code")
+    parser.add_argument("-i", "--intermediate", help="print the intermediate representation", action="store_true")
+    parser.add_argument("-o", "--out", help="path to save compiled file (only with com)",
+                        default="out.sx")
+    args = parser.parse_args()
+    main(args)
+
+
+
+def compile(file, intermediate, out):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(dir_path + "/" + "pyslang.json", 'r') as config_file:
+        config = json.loads(config_file.read())
+
+    with open(file, 'r') as src_file:
+        src_code = src_file.read()
+    macros = Compiler.compile_to_bytecode(src_code, config["stdlib-path"])
+    if intermediate:
+        pprint(macros)
+    path = out
+    save_pickled(macros, path)
+
+
+def main(args):
+
+    if args.command == "com":
+        compile(args.file, args.intermediate, args.out)
+    elif args.command == "exe":
+        macros = open_pickled(args.file)
+        if args.intermediate:
+            pprint(macros)
+        Interpreter.interpret(macros, "main")
+    elif args.command == "run":
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(dir_path + "/" + "pyslang.json", 'r') as config_file:
+            config = json.loads(config_file.read())
+
+        with open(args.file, 'r') as src_file:
+            src_code = src_file.read()
+        macros = Compiler.compile_to_bytecode(src_code, config["stdlib-path"])
+        if args.intermediate:
+            pprint(macros)
+        Interpreter.interpret(macros, "main")
+
+
+
